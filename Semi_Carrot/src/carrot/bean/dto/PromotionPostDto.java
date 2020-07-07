cb3b7d30c6db1fe0145a399382f7207a9b44f914
hjikm11 <hjikm11@naver.com>
2020-07-07 14:13:12 +0900

    board_post_dto 추가

---
+++ Semi_Carrot/src/carrot/bean/dto/PromotionPostDto.java
@@ -0,0 +1,73 @@
+package carrot.bean.dto;
+
+public class PromotionPostDto {
+	private long post_no;
+	private String post_title;
+	private String post_content;
+	private long post_price;
+	private String post_date;
+	private String post_phone;
+	private long post_view;
+	private long post_like;
+	private long promotion_cate_num;
+	
+	public PromotionPostDto() {
+		super();
+	}
+	public long getPost_no() {
+		return post_no;
+	}
+	public void setPost_no(long post_no) {
+		this.post_no = post_no;
+	}
+	public String getPost_title() {
+		return post_title;
+	}
+	public void setPost_title(String post_title) {
+		this.post_title = post_title;
+	}
+	public String getPost_content() {
+		return post_content;
+	}
+	public void setPost_content(String post_content) {
+		this.post_content = post_content;
+	}
+	public long getPost_price() {
+		return post_price;
+	}
+	public void setPost_price(long post_price) {
+		this.post_price = post_price;
+	}
+	public String getPost_date() {
+		return post_date;
+	}
+	public void setPost_date(String post_date) {
+		this.post_date = post_date;
+	}
+	public String getPost_phone() {
+		return post_phone;
+	}
+	public void setPost_phone(String post_phone) {
+		this.post_phone = post_phone;
+	}
+	public long getPost_view() {
+		return post_view;
+	}
+	public void setPost_view(long post_view) {
+		this.post_view = post_view;
+	}
+	public long getPost_like() {
+		return post_like;
+	}
+	public void setPost_like(long post_like) {
+		this.post_like = post_like;
+	}
+	public long getPromotion_cate_num() {
+		return promotion_cate_num;
+	}
+	
+	public void setPromotion_cate_num(long promotion_cate_num) {
+		this.promotion_cate_num = promotion_cate_num;
+	}
+	
+}

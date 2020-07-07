3150b3f2277c08b0c0de55b691c319df958a1971
D0 <dip.0cean@daum.net>
2020-07-07 14:19:37 +0900

    확인 완료 > Dto > DTO 로 이름 통일

--- Semi_Carrot/src/carrot/bean/dto/PromotionPostDTO.java
+++ Semi_Carrot/src/carrot/bean/dto/PromotionPostDTO.java
@@ -1,19 +1,19 @@
 package carrot.bean.dto;
 
-public class UsedPostDto {
+public class PromotionPostDTO {
 	private long post_no;
 	private String post_title;
 	private String post_content;
 	private long post_price;
 	private String post_date;
+	private String post_phone;
 	private long post_view;
 	private long post_like;
-	private long used_cate_num;
-	private long member_no;
-	private long member_img_no;
-	private long addr_no;
-	private long post_img_no;
-	private String post_state;
+	private long promotion_cate_num;
+	
+	public PromotionPostDTO() {
+		super();
+	}
 	public long getPost_no() {
 		return post_no;
 	}
@@ -44,6 +44,12 @@ public class UsedPostDto {
 	public void setPost_date(String post_date) {
 		this.post_date = post_date;
 	}
+	public String getPost_phone() {
+		return post_phone;
+	}
+	public void setPost_phone(String post_phone) {
+		this.post_phone = post_phone;
+	}
 	public long getPost_view() {
 		return post_view;
 	}
@@ -56,44 +62,12 @@ public class UsedPostDto {
 	public void setPost_like(long post_like) {
 		this.post_like = post_like;
 	}
-	public long getUsed_cate_num() {
-		return used_cate_num;
-	}
-	public void setUsed_cate_num(long used_cate_num) {
-		this.used_cate_num = used_cate_num;
-	}
-	public long getMember_no() {
-		return member_no;
-	}
-	public void setMember_no(long member_no) {
-		this.member_no = member_no;
-	}
-	public long getmember_img_no() {
-		return member_img_no;
-	}
-	public void setmember_img_no(long member_img_no) {
-		this.member_img_no = member_img_no;
-	}
-	public long getAddr_no() {
-		return addr_no;
-	}
-	public void setAddr_no(long addr_no) {
-		this.addr_no = addr_no;
-	}
-	public long getPost_img_no() {
-		return post_img_no;
-	}
-	public void setPost_img_no(long post_img_no) {
-		this.post_img_no = post_img_no;
-	}
-	public String getPost_state() {
-		return post_state;
-	}
-	public void setPost_state(String post_state) {
-		this.post_state = post_state;
+	public long getPromotion_cate_num() {
+		return promotion_cate_num;
 	}
 	
-	public UsedPostDto() {
-		super();
+	public void setPromotion_cate_num(long promotion_cate_num) {
+		this.promotion_cate_num = promotion_cate_num;
 	}
+	
 }

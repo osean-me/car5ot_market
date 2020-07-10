ce8277bb6e3586d2bd3ffa60ed07b9ac659246ca
dkfma6238 <areum813@naver.com>
2020-07-10 17:04:18 +0900

    UsedPostDAO 변경

--- Semi_Carrot/src/carrot/bean/dao/UsedPostDAO.java
+++ Semi_Carrot/src/carrot/bean/dao/UsedPostDAO.java
@@ -3,57 +3,60 @@ package carrot.bean.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import carrot.bean.dto.AddrDTO;
 import carrot.bean.dto.UsedPostDTO;
 
 public class UsedPostDAO {
-	
-	private static DataSource src;
-	static {
-		try {
-			Context ctx = new InitialContext();
-			Context env = (Context) ctx.lookup("java:/comp/env");
-			src = (DataSource) env.lookup("jdbc/oracle");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public Connection getConnection() throws Exception{
-		return src.getConnection();
-	}
-	
-	//단일조회
-	public UsedPostDTO get(long post_no)throws Exception{
-		Connection con=getConnection();
-		
-		String sql="Select*from used_post where post_no=?";
-		PreparedStatement ps=con.prepareStatement(sql);
-		ps.setLong(1, post_no);
-		ResultSet rs=ps.executeQuery();
-		
-		UsedPostDTO updto = rs.next() ? new UsedPostDTO(rs) : null;
-		
-		con.close();
-		
-		return updto;
-	}
-	
-	//조회수 증가
-	public void plusViewCount(long post_no, long member_no)throws Exception{
-		Connection con = getConnection();
-		
-		String sql="update used_post set post_view = post_view+1 where post_no=? and member_no != ?";
-		PreparedStatement ps=con.prepareStatement(sql);
-		ps.setLong(1, post_no);
-		ps.setLong(2, member_no);
-		ps.execute();
-		
-		con.close();
-	}
+   private static DataSource src;
+
+   static {
+      try {
+         Context ctx = new InitialContext();
+         Context env = (Context) ctx.lookup("java:/comp/env");
+         src = (DataSource) env.lookup("jdbc/oracle");
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public Connection getConnection() throws Exception {
+      return src.getConnection();
+   }
+
+    //단일조회
+      public UsedPostDTO get(long post_no)throws Exception{
+         Connection con=getConnection();
+         
+         String sql="Select*from used_post where post_no=?";
+         PreparedStatement ps=con.prepareStatement(sql);
+         ps.setLong(1, post_no);
+         ResultSet rs=ps.executeQuery();
+         
+         UsedPostDTO updto = rs.next() ? new UsedPostDTO(rs) : null;
+         
+         con.close();
+         
+         return updto;
+      }
+      
+      //조회수 증가
+      public void plusViewCount(long post_no, long member_no)throws Exception{
+         Connection con = getConnection();
+         
+         String sql="update used_post set post_view = post_view+1 where post_no=? and member_no != ?";
+         PreparedStatement ps=con.prepareStatement(sql);
+         ps.setLong(1, post_no);
+         ps.setLong(2, member_no);
+         ps.execute();
+         
+         con.close();
+      }
+
 }

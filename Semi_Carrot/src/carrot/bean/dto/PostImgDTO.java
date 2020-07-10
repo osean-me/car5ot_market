5b9f070fc71ba8ec048b52cfc3ec753f27aaa270
dkfma6238 <areum813@naver.com>
2020-07-10 14:23:50 +0900

    dto 오타 수정

--- Semi_Carrot/src/carrot/bean/dto/PostImgDTO.java
+++ Semi_Carrot/src/carrot/bean/dto/PostImgDTO.java
@@ -1,19 +1,22 @@
 package carrot.bean.dto;
 
 public class PostImgDTO {
-	private long post_no;
+	private long post_img_no;
 	private String post_img_name;
 	private String post_img;
 	private long post_img_size;
 	public PostImgDTO() {
 		super();
 	}
-	public long getPost_no() {
-		return post_no;
+
+	public long getPost_img_no() {
+		return post_img_no;
 	}
-	public void setPost_no(long post_no) {
-		this.post_no = post_no;
+
+	public void setPost_img_no(long post_img_no) {
+		this.post_img_no = post_img_no;
 	}
+
 	public String getPost_img_name() {
 		return post_img_name;
 	}

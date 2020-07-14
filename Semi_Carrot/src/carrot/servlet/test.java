cd26aabd42efac56b5977c773597fcc2dba31a8a
D0 <dip.0cean@daum.net>
2020-07-14 11:59:58 +0900

    댓글 작성 기능 구

--- Semi_Carrot/src/carrot/servlet/test.java
+++ Semi_Carrot/src/carrot/servlet/test.java
@@ -1,15 +1,31 @@
 package carrot.servlet;
 
-public class test {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-		int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		int count = 0;
-		for (int b : a) {
-			count++;
-			System.out.println("count : " + count + " / b = " + b);
+import carrot.bean.dao.ReplyDAO;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/test.do")
+public class test extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			ReplyDAO rdao = new ReplyDAO();
+
+			long seq_no = rdao.getSequence("test_seq");
+			
+			System.out.println("테스트 시퀀스 번호 : " + seq_no);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}

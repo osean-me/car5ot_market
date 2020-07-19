9235cde91c96e06b017c5844ff47ad0ac567cd3b
hjikm11 <hjikm11@naver.com>
2020-07-19 16:07:31 +0900

    commit

--- Semi_Carrot/src/carrot/servlet/board/PromotionPostEditServlet.java
+++ Semi_Carrot/src/carrot/servlet/board/PromotionPostEditServlet.java
@@ -33,7 +33,7 @@ public class PromotionPostEditServlet extends HttpServlet {
 			/// 1. 수정 페이지에서 등록된 데이터 처리 도구 준비하기
 			String charset = "UTF-8";
 			int limit = 10 * 1024 * 1024;
-			File baseDir = new File("D:/upload/board");
+			File baseDir = new File("F:/upload/board");
 			baseDir.mkdirs();
 
 			// 2. 공장 생성
@@ -75,7 +75,7 @@ public class PromotionPostEditServlet extends HttpServlet {
 				// 5-4. 받아온 예전 이미지 파일 삭제
 				for (PromotionPostImgDTO pidto : old_img_no) {
 					// 파일 본체 삭제
-					File delete_old_img = new File("D:/upload/board/" + pidto.getPost_img_no());
+					File delete_old_img = new File("F:/upload/board/" + pidto.getPost_img_no());
 					delete_old_img.delete();
 				}
 				
